a7d285e3755ef76da14ef967cf2446cb75ab2eef
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-06-22 12:35:54 +0300

    RegisterActivity sexes spinner

--- app/src/main/java/com/example/h2otrack/RegisterActivity.kt
+++ app/src/main/java/com/example/h2otrack/RegisterActivity.kt
@@ -1,12 +1,17 @@
 package com.example.h2otrack
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.h2otrack.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +20,19 @@ class RegisterActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val sexes = resources.getStringArray(R.array.sex)
+        val arrayAdapter = ArrayAdapter(this, R.layout.drop_down_item, sexes)
+        binding.regSex.setAdapter(arrayAdapter)
+
+        val buttonBackToMain = findViewById<ImageButton>(R.id.back_to_main)
+
+
+        buttonBackToMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
