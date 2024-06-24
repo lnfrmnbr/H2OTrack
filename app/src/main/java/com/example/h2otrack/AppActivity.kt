40fec817ee6632487e2f18d560ff8dd548e1cded
Kanyuk Viktoria <spamovik123@yandex.ru>
2024-06-24 22:24:12 +0300

    container selector dialog

--- app/src/main/java/com/example/h2otrack/AppActivity.kt
+++ app/src/main/java/com/example/h2otrack/AppActivity.kt
@@ -1,6 +1,8 @@
 package com.example.h2otrack
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -47,6 +49,11 @@ class AppActivity : AppCompatActivity() {
         }
         replaceFragment(WaterFragment())
         bottomNavigation.show(2)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT){
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
--- app/src/main/java/com/example/h2otrack/fragments/WaterFragment.kt
+++ app/src/main/java/com/example/h2otrack/fragments/WaterFragment.kt
@@ -1,10 +1,19 @@
 package com.example.h2otrack.fragments
 
+import android.app.Dialog
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageButton
+import androidx.annotation.ColorRes
+import androidx.compose.ui.window.Dialog
+import androidx.core.content.ContextCompat
 import com.example.h2otrack.R
 
 class WaterFragment : Fragment() {
@@ -15,5 +24,28 @@ class WaterFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_water, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val buttPlus: Button = view.findViewById<Button>(R.id.plus)
+
+        buttPlus.setOnClickListener {
+            val dialogBinding = layoutInflater.inflate(R.layout.select_container, null)
+            val myDialog = Dialog(requireContext())
+            myDialog.setContentView(dialogBinding)
+            myDialog.setCancelable(true)
+            myDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+            myDialog.show()
+            val buttConfirm=dialogBinding.findViewById<Button>(R.id.butt_select_container)
+            buttConfirm.setOnClickListener {
+                    myDialog.dismiss()
+                }
+            val buttGlass = dialogBinding.findViewById<ImageButton>(R.id.butt_glass)
+            buttGlass.setOnClickListener {
+                buttGlass.setColorFilter(ContextCompat.getColor(requireContext(), R.color.blue_dark))
+            }
+        }
+    }
+
 
 }
